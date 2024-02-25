a90507f5c285fe621160df6ec16750c46b12a173
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-02-25 14:44:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def load_plugins(plugin_name):
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["DEVIL.modules." + plugin_name] = load
+    sys.modules["AUTHOR.modules." + plugin_name] = load
     print("ᴀᴜᴛʜᴏʀ ʜᴀꜱ ɪᴍᴘᴏʀᴛᴇᴅ" + plugin_name)
 
 
